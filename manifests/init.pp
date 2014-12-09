199dd018ab06dac7b93a6a10bdcbdc791b1d9fdb
Garhan Attebury <garhan.attebury@unl.edu>
2014-12-09 14:23:02 -0600

    import straight from oldmodules

---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+#
+# Class: sssd
+#
+
+class sssd (
+    $ldap_uri               = 'ldap://ldap.example.org',
+    $ldap_base              = 'dc=example,dc=org',
+    $ldap_tls_reqcert       = undef,
+    $ldap_tls_cacert        = undef,
+    $ldap_tls_cacertdir     = undef,
+    $ldap_id_use_start_tls  = undef,
+    ) {
+
+    package { 'sssd': ensure => present, }
+
+    service { 'sssd':
+        ensure      => running,
+        name        => 'sssd',
+        enable      => true,
+        hasrestart  => true,
+        hasstatus   => true,
+        require     => Package['sssd'],
+        subscribe   => File['sssd.conf'],
+    }
+
+    file { 'sssd.conf':
+        path    => '/etc/sssd/sssd.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0600',
+        require => Package['sssd'],
+        content => template('sssd/sssd.conf.erb'),
+    }
+
+}
+
